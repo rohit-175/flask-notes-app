0705b93097b63145db3ccf0a6995c927967f636a
Rohit Saseendran <saseendranrohit3002@gmail.com>
2025-07-12 10:41:28 +0530

    handling http req. & flashing msg

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,23 @@ def login():
 def logout():
     return "<p>Logout</p"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('smol email lol', category='error')
+        elif len(firstName) < 2:
+            flash('that a name?', category='error')
+        elif password1 != password2:
+            flash('stick to 1 password ninja!', category='error')
+        elif len(password1) < 7:
+            flash('weak ahh password', category='error')
+        else:
+            flash('GG', category='success')
+        
     return render_template("sign_up.html")
